d41cd7af7a332de736c6c1910f9f601b524de0f2
Gilson-chipombo <fernandochipombo@gmail.com>
2025-06-30 22:29:36 +0100

    Problema no middleware resolvido

--- middleware/AuthMiddleware.php
+++ middleware/AuthMiddleware.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['logado']) || $_SESSION['logado'] !== true) {
     header('Location: ../index.php');
     exit();
 }
